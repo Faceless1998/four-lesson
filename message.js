943bcdbfb2b6c85faf436921fb0793f2b1cef15d
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2025-01-17 17:40:51 +0400

    new

---
+++ message.js
@@ -0,0 +1,16 @@
+// const message = () =>{
+//     const name = "John"
+//     const age = 26
+//     return name + " is " + age + " years old";
+// }
+
+// export default message;
+
+// export const name = "John"
+// export const surname = "Doe"
+
+
+const name = "John"
+const surname = "Doe"
+
+export {name, surname}
