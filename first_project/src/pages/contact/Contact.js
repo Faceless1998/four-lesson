2a111d8a0a950c2d61e6e0bdcd6e7c45b69784b3
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2025-03-06 16:48:29 +0400

    new

--- first_project/src/pages/contact/Contact.js
+++ first_project/src/pages/contact/Contact.js
@@ -1,7 +1,32 @@
 import React from 'react'
 
+const data = [
+  {
+    ID: 87325876,
+    firstName: "hello world",
+    lastName: "ragaca", //String
+    age: 18,  //Number
+    status: true, //Boolean
+    profit: "100L",
+  },
+  {
+    ID: 87325876,
+    firstName: "hello",
+    lastName: "ragaca", //String
+    age: 18,  //Number
+    status: true, //Boolean
+  }
+]
+
+
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+    {
+      data.map( function(element, index) {
+        return <div> {element.firstName} - {element.lastName} - {element.profit} </div>
+      })
+    }
+    </>
   )
 }
--- first_project/src/pages/home/Home.js
+++ first_project/src/pages/home/Home.js
@@ -1,8 +1,11 @@
 import React from "react";
+import stylus from "./home.module.css"
+//stylus
 
 export const Home = () => {
     return (
         <>
+        <div className={stylus.myclass}></div>
             This is sparta
         </>
     )
