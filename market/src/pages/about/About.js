e538957ee142d7c256e0c823e9bd63b75e44a7d2
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2025-03-10 18:14:33 +0400

    new

--- market/src/pages/about/About.js
+++ market/src/pages/about/About.js
@@ -1,9 +1,12 @@
 import React from "react";
- 
+import Ceo from "./Ceo"
+
  export const About = () => {
      return(
          <>
              About
+
+             <Ceo />
          </>
      )
  }
---
+++ market/src/pages/about/Ceo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ceo.css"
+const Ceo = () => {
+    return(
+        <>
+            <div className="card">
+                <div className="name">John Doe</div>
+                <div className="position">CEO</div>
+            </div>
+        </>
+    )
+}
+
+export default Ceo;
