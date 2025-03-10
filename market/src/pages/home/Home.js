61d0f9e28d035014401800fec9d643c3b84e4157
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2025-03-10 17:12:14 +0400

    new

---
+++ market/src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react"
+import stylus from "./home.module.css" 
+
+ export const Home = () => {
+     return(
+         <>
+            <div className={stylus.container}>
+
+            </div>
+
+         </>
+     )
+ }
