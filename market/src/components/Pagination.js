26dd0820bd38003fc894df0aeae3e2e3c0de024d
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2025-04-02 18:02:43 +0400

    new

--- market/src/components/Pagination.js
+++ market/src/components/Pagination.js
@@ -1,27 +1,17 @@
 import React, { useState, useEffect } from "react";
 
-const productsData = [
-  { id: 1, name: "Product 1", price: "$20" },
-  { id: 2, name: "Product 2", price: "$30" },
-  { id: 3, name: "Product 3", price: "$40" },
-  { id: 4, name: "Product 4", price: "$50" },
-  { id: 5, name: "Product 5", price: "$60" },
-  { id: 6, name: "Product 6", price: "$70" },
-  { id: 7, name: "Product 7", price: "$80" },
-  { id: 8, name: "Product 8", price: "$90" },
-  { id: 9, name: "Product 9", price: "$100" },
-  { id: 10, name: "Product 10", price: "$110" },
-  { id: 11, name: "Product 11", price: "$120" },
-  { id: 12, name: "Product 12", price: "$130" },
-];
-
+const productsData = Array.from( { length: 101 }, (_, index) => ({
+    id: index+1,
+    name: `product ${index+1}`,
+    price: `$${ (index+1) * 10 }`
+}))
+ 
 const Pagination = () => {
   const [currentPage, setCurrentPage] = useState(1);
-  const [productPerPage] = useState(4);
+  const [productPerPage] = useState(10);
 
   const indexOfLastProduct = currentPage * productPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productPerPage;
-
   const currentProducts = productsData.slice(
     indexOfFirstProduct,
     indexOfLastProduct
@@ -29,8 +19,35 @@ const Pagination = () => {
 
   const totalPages = Math.ceil(productsData.length / productPerPage);
 
-  const paginate = (pageNumber) => {
-    setCurrentPage(pageNumber);
+  const paginate = () => {
+    const pages = [];
+    const maxVisiblePage = 3;
+
+    if( totalPages <=7){
+      return Array.from( {length: totalPages }, (_, index) => index+1)
+    }
+
+    pages.push(1);
+
+    if(currentPage > maxVisiblePage){
+      pages.push("...")
+    }
+
+    const startPage = Math.max( 2, currentPage - 1 );
+    const endPage = Math.min(totalPages - 1, currentPage + 1);
+    
+    for(let i = startPage; i<=endPage; i++){
+      pages.push(i);
+    }
+
+    if(currentPage < totalPages - maxVisiblePage + 1 ){
+      pages.push("...");
+    }
+
+    pages.push(totalPages)
+
+    return pages;
+
   };
 
   return (
@@ -53,11 +70,15 @@ const Pagination = () => {
           <button onClick={() => paginate(1)}> &lt;&lt; </button>
           <button onClick={() => paginate(currentPage - 1)}> &lt; </button>
 
-          {Array.from({ length: totalPages }, (_, index) => (
-            <button key={index + 1} onClick={() => paginate(index + 1)}>
-              {index + 1}
-            </button>
-          ))}
+          {
+            paginate().map( (page, index) => (
+              page === "..." 
+              ?
+              ( <span key={index}> {page} </span> )
+              :
+              ( <button onClick={() => setCurrentPage(page)}> {page} </button> )
+            ) )
+          }
 
           <button onClick={() => paginate(currentPage + 1)}> &gt; </button>
           <button onClick={() => paginate(totalPages)}> &gt;&gt; </button>
